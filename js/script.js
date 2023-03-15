85bb7e312a0df64065b0372b5839202cfb8ac4bc
Julia Antonova <1306577@mail.ru>
2023-03-15 18:18:31 +0100

    Добавлена валидация поля поиска

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 import { Octokit } from "https://cdn.skypack.dev/octokit";
 
+const octokit = new Octokit({});
 const form = document.forms["search"];
 const repositoriesElement = document.querySelector('.repositories');
 const loader = document.querySelector('.loader');
 
 form.onsubmit = async (event) => {
   event.preventDefault();
-
-  loader.hidden = false;
-  repositoriesElement.innerHTML = '';
   const queryString = form.elements.q.value;
-  const octokit = new Octokit({});
+  const isSearchQueryValid = validateSearchInput(queryString);
 
-  try {
-    const result = await octokit.request("GET /search/repositories", {
-      q: queryString,
-    });
-    loader.hidden = true;
-    const repositories = result.data.items.slice(0, 10);
-    showRepositories(repositories);
-  } catch (error) {
-    loader.hidden = true;
-    handleError(error.status);
+  if (isSearchQueryValid) {
+    loader.hidden = false;
+    repositoriesElement.innerHTML = '';
+    
+    try {
+      const result = await octokit.request("GET /search/repositories", {
+        q: queryString,
+      });
+      loader.hidden = true;
+      const repositories = result.data.items.slice(0, 10);
+      showRepositories(repositories);
+    } catch (error) {
+      loader.hidden = true;
+      handleError(error.status);
+    }
   }
 }
 
@@ -58,4 +61,38 @@ function showMessage(text) {
   repositoriesElement.insertAdjacentHTML('beforeend', `
     <p>${text}</>
   `)
+}
+
+function validateSearchInput(text) {
+  let isValid = false;
+  
+  if (text.length === 0) {
+    showInvalidInputMessage('Задан пустой поисковый запрос');
+  } else if (text.length > 256) {
+    showInvalidInputMessage('Превышено максимальное количество символов (256)');
+  } else {
+    isValid = true;
+  }
+
+  return isValid;
+}
+
+function showInvalidInputMessage(message) {
+  const searchField = form.querySelector('.search__field');
+  const inputElement = searchField.querySelector('.search__input');
+  
+  inputElement.classList.add('search__input_invalid');
+
+  if (!searchField.querySelector('.search-field__message')) {
+    searchField.insertAdjacentHTML('beforeend', `
+    <p class="search-field__message">${message}</p>
+  `)
+  }
+
+  inputElement.oninput = () => {
+    if (inputElement.classList.contains('search__input_invalid')) {
+      inputElement.classList.remove('search__input_invalid');
+      searchField.querySelector('.search-field__message').remove();
+    }
+  }
 }
