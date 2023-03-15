2d30ea2af6e6c196cb324d0176dd980e8a53f606
Julia Antonova <1306577@mail.ru>
2023-03-15 13:35:17 +0100

    Добавлена обработка ошибок с сервера

--- js/script.js
+++ js/script.js
@@ -9,25 +9,53 @@ form.onsubmit = async (event) => {
 
   loader.hidden = false;
   repositoriesElement.innerHTML = '';
-  
   const queryString = form.elements.q.value;
-
   const octokit = new Octokit({});
-  const result = await octokit.request("GET /search/repositories", {
-    q: queryString,
-  });
 
-  if (result.status === 200) {
+  try {
+    const result = await octokit.request("GET /search/repositories", {
+      q: queryString,
+    });
     loader.hidden = true;
+    const repositories = result.data.items.slice(0, 10);
+    showRepositories(repositories);
+  } catch (error) {
+    loader.hidden = true;
+    handleError(error.status);
   }
-  
-  const repositories = result.data.items.slice(0, 10);
+}
 
+function showRepositories(repositories) {
   for (const repository of repositories) {
-    repositoriesElement.insertAdjacentHTML("beforeend", `
+    repositoriesElement.insertAdjacentHTML('beforeend', `
       <div class="repository">
         <a class="repository__link" href="${repository.html_url}" target="_blank">${repository.owner.login}/<em class="repository__name">${repository.name}</em></a>
       </div>
     `)
   }
+}
+
+function handleError(statusCode) {
+  const httpResponseStatus = {
+    'OK': 200,
+    'Validation failed': 422,
+    'Service unavailable': 503,
+  }
+
+  switch (statusCode) {
+      case httpResponseStatus['Validation failed']:
+        showMessage('Некорректный поисковой запрос. Попробуйте еще раз.')
+        break;
+      case httpResponseStatus['Service unavailable']:
+        showMessage('Сервис временно недоступен')
+        break;
+      default:
+        showMessage('Произошла ошибка. Попробуйте еще раз.');
+    }
+}
+
+function showMessage(text) {
+  repositoriesElement.insertAdjacentHTML('beforeend', `
+    <p>${text}</>
+  `)
 }
